cf43f588def407691831ca926c65d3aab34eb041
ChayaSt <chaya.stern@choderalab.org>
2018-09-21 16:18:10 -0400

    import chemi module

--- fragmenter/__init__.py
+++ fragmenter/__init__.py
@@ -8,7 +8,7 @@ Fragment molecules for quantum mechanics torison scans.
 
 # Add imports here
 
-from . import fragment, torsions, workflow_api, utils
+from . import fragment, torsions, workflow_api, utils, chemi
 from .workflow_api import enumerate_states, enumerate_fragments
 
 # Handle versioneer
