01a17d718b2dec243d717ef3f56da33ef6dac869
SimonBoothroyd <simon.boothroyd@hotmail.com>
2021-03-30 12:41:12 +0100

    Refactor stereo enumeration to use the OFF toolkit (#76)

---
+++ fragmenter/tests/test_states.py
@@ -0,0 +1,76 @@
+import pytest
+from openff.toolkit.topology import Molecule
+from openff.toolkit.utils import OpenEyeToolkitWrapper, RDKitToolkitWrapper
+
+from fragmenter.states import _enumerate_stereoisomers
+from fragmenter.tests.utils import global_toolkit_wrapper
+
+
+@pytest.mark.parametrize(
+    "smiles, force_flip, expected",
+    [
+        (
+            "FC(Br)(Cl)[C@@H](Br)(Cl)",
+            True,
+            [
+                "F[C@](Br)(Cl)[C@@H](Br)(Cl)",
+                "F[C@](Br)(Cl)[C@H](Br)(Cl)",
+                "F[C@@](Br)(Cl)[C@@H](Br)(Cl)",
+                "F[C@@](Br)(Cl)[C@H](Br)(Cl)",
+            ],
+        ),
+        (
+            "FC(Br)(Cl)[C@@H](Br)(Cl)",
+            False,
+            ["F[C@](Br)(Cl)[C@@H](Br)(Cl)", "F[C@@](Br)(Cl)[C@@H](Br)(Cl)"],
+        ),
+        ("FC(Cl)Br", False, ["F[C@H](Cl)Br", "F[C@@H](Cl)Br"]),
+        ("F[C@H](Cl)Br", True, ["F[C@H](Cl)Br", "F[C@@H](Cl)Br"]),
+        ("F[C@H](Cl)Br", False, ["F[C@H](Cl)Br"]),
+        ("ClC=CBr", False, [r"Cl/C=C/Br", r"Cl\C=C/Br"]),
+        ("Cl/C=C/Br", False, [r"Cl/C=C/Br"]),
+        ("Cl/C=C/Br", True, [r"Cl/C=C/Br", r"Cl\C=C/Br"]),
+    ],
+)
+@pytest.mark.parametrize(
+    "toolkit_wrapper", [OpenEyeToolkitWrapper(), RDKitToolkitWrapper()]
+)
+def test_enumerate_stereoisomers(smiles, force_flip, expected, toolkit_wrapper):
+
+    if (
+        ("@" in smiles or "/" in smiles)
+        and force_flip
+        and isinstance(toolkit_wrapper, RDKitToolkitWrapper)
+    ):
+
+        pytest.skip(
+            "the rdkit wrapper cannot flip existing stereo. see #892 of the OFF TK."
+        )
+
+    with global_toolkit_wrapper(toolkit_wrapper):
+
+        molecule = Molecule.from_smiles(smiles, allow_undefined_stereo=True)
+
+        stereoisomers = _enumerate_stereoisomers(
+            molecule.to_openeye(),
+            force_flip=force_flip,
+            enum_nitrogen=True,
+            verbose=False,
+        )
+
+    assert len(stereoisomers) == len(expected)
+
+    expected = {
+        Molecule.from_smiles(stereoisomer, allow_undefined_stereo=True).to_smiles(
+            explicit_hydrogens=False, isomeric=True, mapped=False
+        )
+        for stereoisomer in expected
+    }
+    actual = {
+        Molecule.from_openeye(stereoisomer, allow_undefined_stereo=True).to_smiles(
+            explicit_hydrogens=False, isomeric=True, mapped=False
+        )
+        for stereoisomer in stereoisomers
+    }
+
+    assert expected == actual
--- fragmenter/tests/utils.py
+++ fragmenter/tests/utils.py
@@ -1,6 +1,13 @@
 import importlib
+from contextlib import contextmanager
+from typing import Union
 
 import pytest
+from openff.toolkit.utils import (
+    GLOBAL_TOOLKIT_REGISTRY,
+    ToolkitRegistry,
+    ToolkitWrapper,
+)
 
 try:
     importlib.import_module("openeye.oechem")
@@ -10,3 +17,19 @@ except ImportError:
     has_openeye = False
 
 using_openeye = pytest.mark.skipif(not has_openeye, reason="Cannot run without OpenEye")
+
+
+@contextmanager
+def global_toolkit_wrapper(toolkit_wrapper: Union[ToolkitRegistry, ToolkitWrapper]):
+
+    original_toolkits = GLOBAL_TOOLKIT_REGISTRY._toolkits
+
+    if isinstance(toolkit_wrapper, ToolkitRegistry):
+        GLOBAL_TOOLKIT_REGISTRY._toolkits = toolkit_wrapper.registered_toolkits
+
+    else:
+        GLOBAL_TOOLKIT_REGISTRY._toolkits = [toolkit_wrapper]
+
+    yield
+
+    GLOBAL_TOOLKIT_REGISTRY._toolkits = original_toolkits
